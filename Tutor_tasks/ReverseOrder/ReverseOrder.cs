7e863f0168f0a35f3d90506012144777f7947c24
KateZytchuk <24zaets39@gmail.com>
2019-03-15 15:37:15 +0200

    ReverseOrder is done

---
+++ Tutor_tasks/ReverseOrder/ReverseOrder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ReverseOrder
+{
+    class ReverseOrder
+    {
+        static void Main(string[] args)
+        {
+            string inpt_str = string.Empty, term_num = string.Empty, arr_str = string.Empty;
+            int index;
+            string[] addtnl_arr;
+
+            inpt_str = Console.ReadLine();
+
+            index = inpt_str.LastIndexOf(' ');
+
+            term_num = inpt_str.Substring(index + 1);
+
+            arr_str = inpt_str.Substring(0, index);
+            addtnl_arr = arr_str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int[] ArrToRev = new int[addtnl_arr.Length];
+
+            for (int i = 0; i < addtnl_arr.Length; i++)
+            {
+                ArrToRev[i] = int.Parse(addtnl_arr[i]);
+            }
+
+            Array.Reverse(ArrToRev);
+
+            foreach (int i in ArrToRev)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.Write("{0}", term_num);
+            Console.ReadLine();
+        }
+    }
+}
