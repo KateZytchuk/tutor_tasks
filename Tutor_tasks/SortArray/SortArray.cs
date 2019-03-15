d663289a27e02b573ecc588d410becab7883d1c2
KateZytchuk <24zaets39@gmail.com>
2019-03-15 15:57:57 +0200

    SortArray done

---
+++ Tutor_tasks/SortArray/SortArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArray
+{
+    class SortArray
+    {
+        static void Main(string[] args)
+        {
+            string inpt_str = Console.ReadLine();
+            int index = inpt_str.IndexOf(' ');
+            int quant = int.Parse(inpt_str.Substring(0, index));
+            string str_arr = inpt_str.Substring(index);
+            string[] arr_str = str_arr.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int [] workArr = new int[arr_str.Length];
+            for (int i = 0; i < arr_str.Length; i++)
+                workArr[i] = int.Parse(arr_str[i]);
+            Array.Sort(workArr);
+            Console.Write("{0} ", quant);
+            foreach (int i in workArr)
+                Console.Write("{0} ", i);
+            Console.ReadKey();
+        }
+    }
+}
